39891819787f52d400d5c82f7bd6d01cf2fed2f0
HudsonJoe <jdjihaoyu2008@icloud.com>
2017-07-22 17:24:59 +0800

    changing swig_home.html

---
+++ SPAFront/js/login.js
@@ -0,0 +1,18 @@
+// get username and user password
+$().ready(function(){
+	$('#login_btn').on('click', function(e){
+		e.preventDefault();
+		// get user name and user password
+		var $log_name = $('#login_username').val();
+		var $log_password = $('#login_password').val();
+		alert($log_name);
+		alert($log_password);
+
+		//communicate with the server
+
+
+
+		// get the log in result and render the page
+		
+	});
+});
