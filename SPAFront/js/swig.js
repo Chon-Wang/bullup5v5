6ddf1eed625344f9e286115b29e2edc396d832c7
HudsonJoe <jdjihaoyu2008@icloud.com>
2017-08-08 19:45:25 +0800

    update queryres page interfaces

--- SPAFront/js/swig.js
+++ SPAFront/js/swig.js
@@ -2,10 +2,7 @@ var douniu = {};
 
 
 douniu.loadSwigView = function (pageRef, data) {
-	var swig = require('swig');
-myApp.loadSwigView = function (pageRef, data) {
 	var	swig = require('swig');
-
 	return swig.renderFile(pageRef, data || {});
 }; // Loading views/swigTemplate.html 
 
