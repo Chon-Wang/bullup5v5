b4c839befd2708ff65bdc98e1bcc30f268fb5924
guojingming <1043840980@qq.com>
2017-08-21 00:33:17 +0800

    finished inviting front

--- SPAFront/js/invite_friend.js
+++ SPAFront/js/invite_friend.js
@@ -1,3 +1,5 @@
+var inviteSelectedFriendBtnEvent = false;
+
 $("#invite_friend_btn").click(function(){
     var friendListHeadHtml = douniu.loadSwigView('swig_friend_list_head.html', {
         user: userInfo
@@ -5,11 +7,19 @@ $("#invite_friend_btn").click(function(){
     var friendListHtml = douniu.loadSwigView('swig_friend_list.html', {
         user: userInfo
     });
-
-
-    
-
-    //不好用
     $("#user_view").html(friendListHeadHtml);
     $('.friend-list').html(friendListHtml);
+    if(!inviteSelectedFriendBtnEvent){
+        inviteSelectedFriendBtnEvent = true;
+        $("#invite_selected_friend_btn").click(function(){
+            var friendListSize = Number.parseInt($("#friend_list_size_hidden").val());
+            
+            for(var i = 0;i<friendListSize;i++){
+                if($("#friend_" + (i+1) + "_check_box").prop("checked")){//选中
+                    alert($("#friend_" + (i+1) + "_check_box").val());//打印选中的值
+                }
+            }
+        });
+    }
+   
 });
