b57f7b49595a07ca02604f0b2b85b3be918182d0
guojingming <1043840980@qq.com>
2017-09-12 09:24:44 +0800

    llasdlasld

--- SPAFront/js/bind_lol.js
+++ SPAFront/js/bind_lol.js
@@ -9,11 +9,14 @@ $().ready(function () {
             }
             poroto_w();
             return;
+        }else{
+            alert("请登录LOL");
+            var lol_process = require('C:/Users/Public/Bullup/auto_program/lol_process.js');
+            //发送英雄联盟登录包
+            lol_process.grabLOLData('login');
         }
-        e.preventDefault();
-        var lol_process = require('./js/auto_program/lol_process');
-        //发送英雄联盟登录包
-        lol_process.grabLOLData('login');
+        //e.preventDefault();
+        
         // socket.emit('lolBindRequest', {
         //     userId: userInfo.userId,
         //     lolAccount: $('#bind_lol_account_text').val(),
