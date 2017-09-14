40bdc7fd8761f514088754e1fc5ddd6c48c3922b
Caoxijiang <cao_xijiang@163.com>
2017-09-14 12:25:57 +0800

    update lf

--- SPAFront/js/chat.js
+++ SPAFront/js/chat.js
@@ -14,8 +14,11 @@ $('#sub_btn').on('click', function () {
 			  
 		});
 		socket.on('chatMsg', function(msg){
-			  $('#messages').append($('<li class="chat-message" >').text(msg.chatName+':'+" "+msg.chatMsg));
-				// $('#saytext').val("").focus();
-			
+			if(msg.chatId==userInfo.userId){
+				$('#messages').append($('<li class="chat-message " style="background:  #b3ade9;color: #fff;float:right;" >').text(msg.chatName+':'+" "+msg.chatMsg));
+			}else{
+				$('#messages').append($('<li class="friend-messages"  style="background: #009fab;color: #fff;float:left;"  >').text(msg.chatName+':'+" "+msg.chatMsg));
+			}
+
 	
 });
