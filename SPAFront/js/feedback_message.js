42ead84fc37e08fafefbe08eff34719664006484
Caoxijiang <cao_xijiang@163.com>
2017-09-14 11:49:09 +0800

    update

--- SPAFront/js/feedback_message.js
+++ SPAFront/js/feedback_message.js
@@ -6,7 +6,7 @@
         var $email=$('#emails').val();
         var $textarea1=$('#textarea1').val();
         if(userInfo==null){
-            bullup.alert("提示:","您没有登陆请登录");
+            alert("提示:","您没有登陆请登录");
         }else{
             socket.emit('feedbackMessage',{
                 name:$name,
