32d9cacf0c269e16dfa6602ad6ee7f1b94613122
guojingming <1043840980@qq.com>
2017-09-16 21:57:03 +0800

    bullup beta

--- SPAFront/js/socket_util.js
+++ SPAFront/js/socket_util.js
@@ -1,5 +1,5 @@
 var io = require('socket.io-client');
-var socket = io.connect('http://127.0.0.1:3000');
+var socket = io.connect('http://67.216.196.197:3000');
 var auto_script = require('./js/auto_program/lol_auto_script');
 var lol_process = require('C:/Users/Public/Bullup/auto_program/lol_process');
 
