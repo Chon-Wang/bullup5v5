dd6c12719d722b72ffc1ce70043e26a4306bdf2b
Caoxijiang <cao_xijiang@163.com>
2017-09-16 23:04:12 +0800

    'jx'

--- SPAFront/js/payment.js
+++ SPAFront/js/payment.js
@@ -4,16 +4,17 @@ var request = require("request");
 
 $(document).ready(function(){
     $("#router_index").click(function(e){
-       
-        var options = {
-            url: 'http://localhost:3001',
-        };
-        request(options, function(error, response, body){
-            var bodyStartIndex = body.indexOf("<body>");
-            var bodyEndIndex = body.indexOf("</body>");
-            var htmlStr = body.substr(0, bodyEndIndex);
-            htmlStr = htmlStr.substr(bodyStartIndex + 6, htmlStr.length - 6);
-            $('#payment').html(htmlStr);
-        });
+        
+        
+        // var options = {
+        //     url: 'http://67.216.196.197:3001',
+        // };
+        // request(options, function(error, response, body){
+        //     var bodyStartIndex = body.indexOf("<body>");
+        //     var bodyEndIndex = body.indexOf("</body>");
+        //     var htmlStr = body.substr(0, bodyEndIndex);
+        //     htmlStr = htmlStr.substr(bodyStartIndex + 6, htmlStr.length - 6);
+        //     $('#payment').html(htmlStr);
+        // });
     })
 });
