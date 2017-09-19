544ab17074de47f6de377e874e0d2bab794ef4e6
JM.Guo <1043840980@qq.com>
2017-09-19 17:26:11 +0800

    befor

--- SPAFront/js/test_page.js
+++ SPAFront/js/test_page.js
@@ -1,8 +1,22 @@
 var lolProcess = require('C:/Users/Public/Bullup/auto_program/lol_process.js');
 var child_process = require("child_process");
+var request = require("request");
 
 $(document).ready(function(){
     $("#router_test_page").click(function(e){
+
+        var options = {
+            url: 'http://localhost:3000',
+        };
+        request(options, function(error, response, body){
+            var bodyStartIndex = body.indexOf("<body>");
+            var bodyEndIndex = body.indexOf("</body>");
+            var htmlStr = body.substr(0, bodyEndIndex);
+            htmlStr = htmlStr.substr(bodyStartIndex + 6, htmlStr.length - 6);
+            $('#main-view').html(htmlStr);
+        });
+
+
         // lolProcess.grabLOLData('result', socket);
         // function processResultPacket(stdout){
         //     var resultPacket = {};
