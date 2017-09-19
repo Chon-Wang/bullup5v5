168bd2161f00c99fba92522aaca0f7512a91a561
Git-Wangchong <907634055@qq.com>
2017-09-19 16:01:06 +0800

    路由路由

--- SPAFront/js/await.js
+++ SPAFront/js/await.js
@@ -1,32 +1,21 @@
 
 $().ready(function(){
+    //我发起的赛事路由
     $('.await_href').on('click', function(e){
        
         e.preventDefault();
-        bullup.loadTemplateIntoTarget('swig_await.html', {}, 'main-view');
-        //动态加载查看队员子页面路由js
+        bullup.loadTemplateIntoTarget('swig_launchevent.html', {}, 'main-view');
+        
         $.getScript('/js/check.js');
-      
-       
-      
-        
-        });
-        $('.ceshi').on('click', function(e){
-            
-             e.preventDefault();
-             bullup.loadTemplateIntoTarget('swig_participateinevents.html', {}, 'main-view');
-             //动态加载查看队员子页面路由js
-             $.getScript('/js/check.js');
-           
-            
-           
-             
-             });
-    
-        
-
-	
     });
+    //我参与的赛事路由
+    $('.routing').on('click', function(e){
+        e.preventDefault();
+        bullup.loadTemplateIntoTarget('swig_participateinevents.html', {}, 'main-view');
+            
+         $.getScript('/js/check.js');
+     });
+  });
    
 
      
--- SPAFront/js/check.js
+++ SPAFront/js/check.js
@@ -1,14 +1,54 @@
 
 $().ready(function(){
-    $('.check').on('click', function(e){
+    $('.registration').on('click', function(e){
        
         e.preventDefault();
-        //查看队员路由
-        bullup.loadTemplateIntoTarget('checkthenumberofapplicants.html', {}, 'main-view');
+        //报名中路由
+        bullup.loadTemplateIntoTarget('swig_signup.html', {}, 'main-view');
       
        
      });
-  });
+     $('.underway').on('click', function(e){
+        
+         e.preventDefault();
+         //进行中路由
+         bullup.loadTemplateIntoTarget('swig_playbyplay.html', {}, 'main-view');
+         $.getScript('/js/createateam.js');
+        
+      });
+      $('.await').on('click', function(e){
+        
+         e.preventDefault();
+         //等待报名路由
+         bullup.loadTemplateIntoTarget('swig_waittosignup.html', {}, 'main-view');
+      
+        
+      });
+      $('.awaygame').on('click', function(e){
+        
+         e.preventDefault();
+         //等待比赛路由
+         bullup.loadTemplateIntoTarget('swig_await.html', {}, 'main-view');
+         $.getScript('/js/createateam.js');
+        
+      });
+      $('.finish').on('click', function(e){
+        
+         e.preventDefault();
+         //已结束路由
+         bullup.loadTemplateIntoTarget('swig_finished.html', {}, 'main-view');
+         $.getScript('/js/createateam.js');
+        
+      });
+      $('.personnelaudit').on('click', function(e){
+        
+         e.preventDefault();
+         //报名审核理由
+         bullup.loadTemplateIntoTarget('swig_audit.html', {}, 'main-view');
+        
+        
+      });
+    });
    
 
      
