7e2ec8a04cb4a03777884e67003cd60b0eb3333e
Git-Wangchong <907634055@qq.com>
2017-09-23 16:06:41 +0800

    排行榜样式优化

--- SPAFront/js/cjdwly.js
+++ SPAFront/js/cjdwly.js
@@ -5,16 +5,12 @@ $().ready(function(){
         e.preventDefault();
         bullup.loadTemplateIntoTarget('chatroom.html', {}, 'main-view');
         $.getScript('./js/chat.js');
-      
-        
-	    });
+    });
         
         $('#return').on('click', function(e){
-       
         e.preventDefault();
         bullup.loadTemplateIntoTarget('swig_index.html', {}, 'main-view');
-      
-         $.getScript('/js/zymly.js');
+        $.getScript('/js/zymly.js');
          $.getScript('/js/Withdraw.js');
 	    });
         
