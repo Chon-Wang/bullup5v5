1af71007715ebfe6cda3337b948195cfe603afb2
guojingming <1043840980@qq.com>
2017-09-30 23:36:21 +0800

    latest

--- SPAFront/js/lolutil.js
+++ SPAFront/js/lolutil.js
@@ -2,7 +2,7 @@ var request = require('request');
 var async = require('async');
 var lolcfg = require('./lolcfg.js');
 
-var apiKey =  "RGAPI-ff74081f-8f30-4fbb-af0f-b2d66eedbf8f";
+var apiKey =  "RGAPI-aec98ade-b04f-4cbf-875d-6f4672116b60";
 
 function getItemsStaticData(callback){
     var options = {
