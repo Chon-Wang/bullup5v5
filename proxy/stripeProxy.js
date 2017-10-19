bde61499869ab2a9fd14f3daffb2d8c4c524e33e
guojingming <1043840980@qq.com>
2017-10-19 12:21:49 +0800

    latest

--- proxy/stripeProxy.js
+++ proxy/stripeProxy.js
@@ -24,7 +24,7 @@ exports.recharge = function(){
         data = data.replace("userNameValue", String(userId));
         fs.writeFileSync('./stripe_views/temp.hbs', data);
         //每次合并代码应将此路径改为自己的
-        res.sendFile('C:/Users/Administrator/Desktop/bullup5v5/stripe_views/temp.hbs');
+        res.sendFile('./stripe_views/temp.hbs');
         //res.sendFile('C:/Users/JM.Guo/Desktop/Stripe/views/index.hbs');
     });
     
